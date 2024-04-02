4876f879fb21ac33646242286b61dd7081aee76d
David_drgn <72632974+David-drgn@users.noreply.github.com>
2024-04-02 00:59:06 +0000

    Meio exercicio

--- Ex9/index.js
+++ Ex9/index.js
@@ -1,5 +1,30 @@
 const readXlsxFile = require('read-excel-file/node');
 
-readXlsxFile('./files/dados.xlsx').then(linhas => {
-    console.log(linhas);
+readXlsxFile('./arquivo/aluno.xlsx').then(linhas => {
+    let razao = []
+    for (let i = 1; i < linhas.length; i++) {
+        const element = linhas[i];
+        let aluno = razao.find(e => e.ra === element[0])
+        if (aluno == undefined) {
+            if (element[2] == 'C') {
+                razao.push({
+                    ra: element[0],
+                    R: [],
+                    C: [element[1]]
+                });
+            }
+            else {
+                razao.push({
+                    ra: element[0],
+                    R: [element[1]],
+                    C: []
+                });
+            }
+        }
+        else {
+            if (element[2] == 'C') aluno.C.push(element[1])
+            else aluno.R.push(element[1])
+        }
+    }
+    console.log(razao)
 });
