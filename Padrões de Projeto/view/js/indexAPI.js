a25f7b92f0c71a93009bb4d1494825ce1ae66d54
David_drgn <72632974+David-drgn@users.noreply.github.com>
2024-05-21 00:23:33 +0000

    commit

--- Padrões de Projeto/view/js/indexAPI.js
+++
@@ -1,5 +0,0 @@
-function openPage() {
-    document.getElementById('hero').style.display = "none"
-    document.getElementById('page1').style.display = "flex"
-    document.getElementById('header').style.display = "flex"
-}
