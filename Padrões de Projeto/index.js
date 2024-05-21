6e9d942ae3d68e181774c74b1623cba42d208f3e
David_drgn <72632974+David-drgn@users.noreply.github.com>
2024-05-21 00:20:58 +0000

    teste

--- Padrões de Projeto/index.js
+++ Padrões de Projeto/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-app.use(bodyParser.json({ limit: "8mb" }));
 
 app.get("/", (_req, res) => {
     res.sendFile(path.join(__dirname, "view", "index.html"));
 });
+
 app.use("/css", express.static(path.join(__dirname, "view", "css")));
 app.use("/js", express.static(path.join(__dirname, "view", "js")));
 
