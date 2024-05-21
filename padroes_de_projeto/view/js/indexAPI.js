1fb7bce67c1b4a91f51e32e8c2b2984fd2e2d70f
David_drgn <72632974+David-drgn@users.noreply.github.com>
2024-05-21 00:23:48 +0000

    more

---
+++ padroes_de_projeto/view/js/indexAPI.js
@@ -0,0 +1,5 @@
+function openPage() {
+    document.getElementById('hero').style.display = "none"
+    document.getElementById('page1').style.display = "flex"
+    document.getElementById('header').style.display = "flex"
+}
